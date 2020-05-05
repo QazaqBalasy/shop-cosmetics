2950ca9d21e1a76fb23f2ac3e4b7f71f0e18ca8e
Sundet Mukhtar <170107052@stu.sdu.edu.kz>
2020-05-05 23:30:50 +0600

    Updated settings

--- test2/settings.py
+++ test2/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o@4-)%(cdjx)n^gt3^^t7k*ai(^+q7d2*gryf9^-(v*#p)@&ap'
-
+#SECRET_KEY = 'o@4-)%(cdjx)n^gt3^^t7k*ai(^+q7d2*gryf9^-(v*#p)@&ap'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cosmetic-shopapp.herokuapp.com']
 
 
 # Application definition
