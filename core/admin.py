471c70af86a511159e9cf359b94007c072aec97a
Sundet Mukhtar <170107052@stu.sdu.edu.kz>
2020-05-05 23:56:10 +0600

    Updated admin.py

--- core/admin.py
+++ core/admin.py
@@ -7,10 +7,14 @@ class UserFavAdmin(admin.ModelAdmin):
     list_display = ['user', 'cosmetics', 'add_time']
     list_filter = ['user', 'cosmetics', 'add_time']
     search_fields = ['user', 'cosmetics']
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['profile','date_added','confirmation_date']
+    list_filter = ['profile','date_added','confirmation_date']
+    search_fields = ['profile']
 
 admin.site.register(Cosmetic)
 admin.site.register(CosmeticCategory)
 admin.site.register(Basket)
-admin.site.register(Order)
+admin.site.register(Order,OrderAdmin)
 admin.site.register(UserFav,UserFavAdmin)
 admin.site.register(BankCard)
