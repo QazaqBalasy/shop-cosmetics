184ca97d6c6faf0d1c5c92726f00e976b002611f
Sundet Mukhtar <170107052@stu.sdu.edu.kz>
2020-05-06 00:43:39 +0600

    changed data in DB

--- core/admin.py
+++ core/admin.py
@@ -7,14 +7,22 @@ class UserFavAdmin(admin.ModelAdmin):
     list_display = ['user', 'cosmetics', 'add_time']
     list_filter = ['user', 'cosmetics', 'add_time']
     search_fields = ['user', 'cosmetics']
+
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['profile','date_added','confirmation_date']
     list_filter = ['profile','date_added','confirmation_date']
     search_fields = ['profile']
 
+
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ['order','price']
+    list_filter = ['order','price']
+
+
 admin.site.register(Cosmetic)
 admin.site.register(CosmeticCategory)
-admin.site.register(Basket)
+admin.site.register(Basket,BasketAdmin)
 admin.site.register(Order,OrderAdmin)
 admin.site.register(UserFav,UserFavAdmin)
 admin.site.register(BankCard)
