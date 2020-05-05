6c56e2166bd0e10c69a2efa7efc4e7158dfe911d
Sundet Mukhtar <170107052@stu.sdu.edu.kz>
2020-05-06 00:45:18 +0600

    fixed settings

--- test2/settings.py
+++ test2/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'o@4-)%(cdjx)n^gt3^^t7k*ai(^+q7d2*gryf9^-(v*#p)@&ap'
 #print(os.environ)
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
